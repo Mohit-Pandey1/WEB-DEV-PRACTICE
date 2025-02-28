613d92b11a35315ef2fffcddf76baeba46150234
m18 <mohit.pandey8910@gmail.com>
2025-02-28 17:31:39 +0530

    Gsap Animation

---
+++ JS ANIMATIONS/CONCEPT/script.js
@@ -0,0 +1,41 @@
+// to used for animating object from it's initial position to final position
+// gsap.to("#box",{
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     rotate:360,
+//     backgroundColor: "blue"
+// })
+
+
+
+// from used for animating object from it's final position to initial position
+// gsap.from("#box",{
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     rotate:360,
+//     backgroundColor: "blue"
+// })
+
+var t = gsap.timeline()
+
+t.to("#box1",{
+    x: 1200,
+    rotate: 360,
+    scale:0.5,
+    duration:2,
+    delay:1
+})
+t.to("#box2",{
+    x: 1200,
+    rotate: 360,
+    scale:0.5,
+    duration:2,
+})
+t.to("#box3",{
+    x: 1200,
+    rotate: 360,
+    scale:0.5,
+    duration:2,
+})
---
+++ JS ANIMATIONS/PROJECT/gsap.js
@@ -0,0 +1,9 @@
+gsap.from("#nav h3", {
+    y:-50,
+    opacity: 0,
+    delay: 0.5,
+    durationn: 0.8,
+    stagger: 0.2
+
+})
+
