b59893e6e3c06a9439449cdcbf6debdb31095022
m18 <mohit.pandey8910@gmail.com>
2025-03-08 17:10:02 +0530

    JS PROJECT

--- JS ANIMATIONS/LOCOMOTIVE/script.js
+++ JS ANIMATIONS/LOCOMOTIVE/script.js
@@ -1,4 +1,20 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+gsap.from("#page1 #box", {
+    scale: 0,
+    duration: 1,
+    delay:1,
+    opacity:0,
+    rotate: 360
+})
+
+gsap.from("#page1 #box", {
+    scale: 0,
+    duration: 1,
+    opacity:0,
+    rotate: 360,
+    scrollTrigger:{
+        trigger: "#page2",
+        scroller: "body",
+        start: "top 50%",
+        markers: true
+    }
+})
