e0b969eead8eaf69aa7af9c67f8729a1f52b4d83
m18 <mohit.pandey8910@gmail.com>
2025-07-06 23:39:52 +0530

    OBYS AGENCY CLONE

--- OBYSS AGENCY CLONE/script.js
+++ OBYSS AGENCY CLONE/script.js
@@ -1,44 +1,53 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
+  var tl = gsap.timeline();
 
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.25,
-  duration: 0.6,
-  delay: 0.5,
-});
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
 
-tl.from("#line1-part1, .line h2", {
-  opacity: 0,
-  onStart: function () {
-    var h5timer = document.querySelector("#line1-part1 h5");
-    var grow = 0;
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
 
-    setInterval(function () {
-      if (grow < 100) {
-        grow++;
-        h5timer.innerHTML = grow++;
-      } else {
-        grow = 100;
-        h5timer.innerHTML = grow;
-      }
-    }, 35);
-  },
-});
+      setInterval(function () {
+        if (grow < 100) {
+          grow++;
+          h5timer.innerHTML = grow++;
+        } else {
+          grow = 100;
+          h5timer.innerHTML = grow;
+        }
+      }, 35);
+    },
+  });
 
-tl.to("#loader", {
-  opacity: 0,
-  duration: 0.4,
-  delay: 3.5,
-});
+  tl.to(".line h2", {
+    animationName: "anime",
+    opacity: 1,
+  });
 
-tl.from("#page1", {
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 3.5,
+  });
+
+  tl.from("#page1", {
     delay: 0.2,
-    y:1600,
+    y: 1600,
     opacity: 0,
     duration: 0.5,
-    ease: Power4
-})
+    ease: Power4,
+  });
 
-tl.to("#loader",{
-    display: "none"
-})
+  tl.to("#loader", {
+    display: "none",
+  });
+}
+
+loadingAnimation()
