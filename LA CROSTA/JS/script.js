6019db4367a1845319f9e08ce10ef83a2f9e49b9
m18 <mohit.pandey8910@gmail.com>
2025-08-27 23:31:24 +0530

    LA CROSTA

--- LA CROSTA/JS/script.js
+++ LA CROSTA/JS/script.js
@@ -1,48 +1,19 @@
-// SHOW MENU
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle')
-      navClose = document.getElementById('nav-close')
+/*=============== SHOW MENU ===============*/
 
-if(navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
-}
 
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-}
+/*=============== REMOVE MENU MOBILE ===============*/
 
-// REMOVE MENU MOBILE
-const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
+/*=============== ADD SHADOW HEADER ===============*/
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
 
-// ADD SHADOW HEADER
-const shadowHeader = () => {
-    const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('shadow-header')
-                       : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', shadowHeader)
+/*=============== SWIPER POPULAR ===============*/
 
-// SWIPER POPULAR
-const swiperPopular = new Swiper('.popular__swiper', {
-  loop: true,
-  grabCursor: true,
-  slidesPerView: 'auto',
-  centeredSlides: true,
-});
 
-// SHOW SCROLL UP
+/*=============== SHOW SCROLL UP ===============*/ 
 
-// SCROLL SECTIONS ACTIVE LINK
 
-// SCROLL REVEAL ANIMATION
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
