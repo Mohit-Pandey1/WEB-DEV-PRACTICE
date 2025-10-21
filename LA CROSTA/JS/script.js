9433dc3acfc4271a25e4e2a3c31ad8da0cf53b7d
m18 <mohit.pandey8910@gmail.com>
2025-10-22 00:24:00 +0530

    LA CROSTA

--- LA CROSTA/JS/script.js
+++ LA CROSTA/JS/script.js
@@ -4,25 +4,25 @@ const navMenu = document.getElementById('nav-menu'),
       navClose = document.getElementById('nav-close')
 
 if(navToggle){
-    navToggle.addEventListener('click', () =>{
+    navToggle.addEventListener('click', ()=>{
         navMenu.classList.add('show-menu')
     })
 }
 
 if(navClose){
-    navClose.addEventListener('click', () =>{
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
 
-
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
